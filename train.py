8244cff5b78f55abda07b1bf735c614f34f45f69
Mahjabin Binte Mannan <78349997+Mahjabin08@users.noreply.github.com>
2023-08-11 21:42:31 +0600

    Update train.py
    train.py was updated with added comment

--- train.py
+++ train.py
@@ -46,7 +46,7 @@ for (pattern_sentence, tag) in xy:
     # X: bag of words for each pattern_sentence
     bag = bag_of_words(pattern_sentence, all_words)
     X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
+    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot.
     label = tags.index(tag)
     y_train.append(label)
 
